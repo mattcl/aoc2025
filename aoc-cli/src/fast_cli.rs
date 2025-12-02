cc07917b9ec8978f0b71980288593087f5ff8985
Matt Chun-Lum <matt@questionable.engineering>
2025-12-02 03:10:21 -0800

    fix: add missing file

---
+++ aoc-cli/src/fast_cli.rs
@@ -0,0 +1,32 @@
+use std::env;
+
+use aoc_plumbing::Problem;
+use gift_shop::GiftShop;
+use secret_entrance::SecretEntrance;
+// import_marker
+
+macro_rules! generate_cli {
+    ($(($name:ident, $day:literal)),* $(,)?) => {
+        pub fn run() -> anyhow::Result<()> {
+            let day: u8 = env::var("AOC_DAY")?.parse()?;
+            let input_file = env::var("AOC_INPUT")?;
+            let input = std::fs::read_to_string(&input_file)?;
+            let out = match day {
+                $(
+                $day => serde_json::to_string(&$name::solve(&input)?)?,
+                )*
+                _ => "\"not implemented\"".into(),
+            };
+
+            println!("{}", out);
+
+            Ok(())
+        }
+    }
+}
+
+generate_cli! {
+    (SecretEntrance, 1),
+    (GiftShop, 2),
+    // command_marker
+}
